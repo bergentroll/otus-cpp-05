61b2560cc4f33b2b2d14d21af7e9ae21acc5627b
Anton Karmanov <bergertroll@insiberia.net>
2020-05-24 20:28:34 +0500

    Implement Canvas::deleteShape()

--- inc/main_window.h
+++ inc/main_window.h
@@ -1,8 +1,10 @@
 #ifndef TINTENLANDSCHAFT_MAIN_WINDOW_H
 #define TINTENLANDSCHAFT_MAIN_WINDOW_H
 
+#include <algorithm>
 #include <iostream>
 #include <memory>
+#include <unordered_map>
 #include <string>
 
 #include "sketch.h"
@@ -24,14 +26,38 @@ namespace tintenschaft {
       std::cout << "Show Canvas" << std::endl;
     }
 
-    void addShape(std::shared_ptr<Shape> shape) {
+    unsigned int addShape(std::shared_ptr<Shape> shape) {
       std::cout << "Add Shape to Canvas" << std::endl;
+      shapes[currentUid] = shape;
       shape->show();
+      return currentUid++;
+    }
+
+    void deleteShape(unsigned int uid) {
+      std::cout << "Delete Shape with uid " << uid << " from Canvas" << std::endl;
+      shapes.erase(uid);
+    }
+
+    void deleteShape(std::shared_ptr<Shape> shape) {
+      auto it {
+        std::find_if(
+          shapes.begin(),
+          shapes.end(),
+          [shape](auto const &item){ return item.second == shape; })
+      };
+      if (it != shapes.end()) {
+      std::cout << "Delete Shape with uid " << it->first << " from Canvas" << std::endl;
+        shapes.erase(it);
+      } else {
+        std::cout << "Failed to delete Shape from Canvas" << std::endl;
+      }
     }
 
   private:
     Sketch sketch { };
     Color backgroundColor { colors::WHITE };
+    unsigned int currentUid { 1 };
+    std::unordered_map<unsigned int, std::shared_ptr<Shape>> shapes;
   };
 
   class MainWindow: public Widget {
--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,15 @@ int main() {
   doc.newFile("Mono Lisa.svg");
 
   canvas.setBackgroundColor(Color(colors::WHITE));
-  std::shared_ptr<Ellipse> ellipse {
-    new Ellipse(Coordinate{20, 20}, Coordinate{80, 80}, colors::BLACK) };
-  canvas.addShape(ellipse);
+  std::shared_ptr<Ellipse> face { new Ellipse({20, 0}, {80, 100}, colors::BLACK) };
+  std::shared_ptr<Ellipse> leftEye { new Ellipse({30, 30}, {40, 40}, colors::BLACK) };
+  std::shared_ptr<Ellipse> rightEye { new Ellipse({60, 30}, {70, 40}, colors::BLACK) };
+
+  canvas.addShape(face);
+  canvas.addShape(leftEye);
+  auto rightEyeUid { canvas.addShape(rightEye) };
+
+  canvas.deleteShape(rightEyeUid);
 
   doc.saveFile();
 }
